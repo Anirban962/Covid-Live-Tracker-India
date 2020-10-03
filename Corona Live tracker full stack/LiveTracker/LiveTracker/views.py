172d234b8c4ca551224d877d4099b59008a6e719
Anirban Das <aviseksen369@gmail.com>
2020-10-03 22:15:23 +0530

    change in views.py

--- Corona Live tracker full stack/LiveTracker/LiveTracker/views.py
+++ Corona Live tracker full stack/LiveTracker/LiveTracker/views.py
@@ -1,6 +1,4 @@
-# I have to create views.py. Initially it doesn't exist while creating project.
 
-from django.http import HttpResponse
 from django.shortcuts import render
 import requests
 from bs4 import BeautifulSoup
@@ -10,13 +8,13 @@ def index(request):
     html_data = requests.get("https://www.mohfw.gov.in/index.html").text
     # print(html_data)
     soup = BeautifulSoup(html_data, 'html.parser')
-    info_name = soup.find('li', class_="bg-blue").find('span', class_='mob-show').get_text()
+    #info_name = soup.find('li', class_="bg-blue").find('span', class_='mob-show').get_text()
     info_num = soup.find('li', class_="bg-blue").contents[3].get_text(strip=True)
 
-    info_name1 = soup.find('li', class_="bg-green").find('strong', class_='mob-hide').get_text()
+    #info_name1 = soup.find('li', class_="bg-green").find('strong', class_='mob-hide').get_text()
     info_num1 = soup.find('li', class_="bg-green").contents[3].get_text(strip=True)
 
-    info_name2 = soup.find('li', class_="bg-red").find('strong', class_='mob-hide').get_text()
+    #info_name2 = soup.find('li', class_="bg-red").find('strong', class_='mob-hide').get_text()
     info_num2 = soup.find('li', class_="bg-red").contents[3].get_text(strip=True)
 
     return render(request, 'index.html', {'info_num': info_num, 'info_num1': info_num1, 'info_num2': info_num2})
