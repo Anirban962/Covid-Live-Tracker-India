52fa2528a72d7333e194d4997c30f829457b78c3
Anirban Das <aviseksen369@gmail.com>
2020-10-06 22:21:55 +0530

    New commit

--- Corona Live tracker full stack/LiveTracker/LiveTracker/settings.py
+++ Corona Live tracker full stack/LiveTracker/LiveTracker/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '-z$gv8rgad)7n0pd6hsl^6k!)v*2u98(ouszj)y+)70fr@lkp#'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1']
 
 
 # Application definition
